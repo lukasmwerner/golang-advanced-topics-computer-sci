878604f4c13dddbb1d3b02e3e4fc59cba2fee140
Lukas Werner <lwerner@lshigh.org>
2020-10-02 22:19:40 -0700

    updates

---
+++ ch3/mandelbrot/mandelbrot.go
@@ -0,0 +1,16 @@
+//NOT FINISHED
+
+package main
+
+import "image"
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py) / height * (ymax)
+	}
+}
